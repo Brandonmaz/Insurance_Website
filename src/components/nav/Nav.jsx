bc52be5d0ce2ea9d64380548a349c8b2f164ef5d
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2022-10-20 19:26:25 -0700

    created sections and navbar with interaction

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {GiBookmark} from 'react-icons/gi'
+import {TbHeartHandshake} from 'react-icons/tb'
+import {FiPhoneCall} from 'react-icons/fi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const[activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={()=> setActiveNav('#')}className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a> 
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiBookmark/></a>
+      <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><TbHeartHandshake/></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><FiPhoneCall/></a>
+    </nav>
   )
 }
 
