6ad9db131b54d2b8b0a8ae3a48cdd5b1b4bd7828
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2022-10-22 11:06:36 -0700

    created a few sections and finished the css

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,112 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* box 1 experience */}
+
+        <div className="experience__backend">
+          <h3>Backend</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Skill</h4>
+                <small className='text-light'>experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* box 2 experience */}
+        </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/certification.png'
 
+// map array for portfolio article items
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Work 1',
+    github: 'http://github.com',
+    demo: 'http://github.com',
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Work 2',
+    github: 'http://github.com',
+    demo: 'http://github.com',
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Work 3',
+    github: 'http://github.com',
+    demo: 'http://github.com',
+  },
+]
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                  <h3>{title}</h3>
+                  <div className="portfolio__item-cta">
+                    <a href={github} className="btn" target='_blank'>Github</a>
+                    <a href={demo} className="btn btn-primary" target='_blank'>Github</a>
+                  </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
